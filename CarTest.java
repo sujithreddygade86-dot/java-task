0e10475953374fb8abb1b2e5eaf6d8810ef6976c
Sujith Reddy <sujithreddygade86@gmail.com>
2026-06-17 15:29:28 +0530

    initial commit

--- CarTest.java
+++ CarTest.java
@@ -1,5 +1,26 @@
 package com.exceptions.in;
 
+class CarStopped extends Exception {
+
+    public CarStopped(String message) {
+        super(message);
+    }
+}
+
+class CarPuncture extends Exception {
+
+    public CarPuncture(String message) {
+        super(message);
+    }
+}
+
+class CarHeat extends Exception {
+
+    public CarHeat(String message) {
+        super(message);
+    }
+}
+
 public class CarTest {
 
     public static void stop(String value) {
